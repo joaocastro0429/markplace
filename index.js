da39cf020fcd8688ac628d59b01b466126f55d65
joaocastro0429 <joaocastro0429@gmail.com>
2025-08-30 19:06:58 -0300

    projeto marketplace setup inicial

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require ('express')
+const db= require('./src/database/db')
 
 const app = express()
+db.connect()
 
 app.get('/', (req,res)=>{
     res.send({message:"Bem vindo ao nosso market-place!"})
 })
 
 app.listen(3000)
+
+
